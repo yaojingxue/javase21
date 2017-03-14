d38f88386fb21c6fee7d5c6924bb4d9cf7cf0791
yaojingxue <yaojingxuefendou@163.com>
2017-03-14 19:29:31 +0800

    3.14 IDEcommit

---
+++ web/src/com/xinshou/web/HomeServlet.java
@@ -0,0 +1,24 @@
+package com.xinshou.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by acer on 2017/3/13.
+ */
+@WebServlet("/omeservlet")
+public class HomeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println(" word!");
+        response.sendRedirect("/home.jsp");
+    }
+}
---
+++ web/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: acer
+  Date: 2017/3/13
+  Time: 22:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <a href="/homeservlet">ooo</a>
+  </body>
+</html>
